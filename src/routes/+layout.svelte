4b9df0d54ae77008d6e24e022eea6120b34defd9
OstadOmid <ostadomid@users.noreply.github.com>
2023-04-23 12:06:20 +0430

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">Who am I</a></li>
+	</ul>
+</nav>
+<slot />
+
+<style>
+	nav {
+		width: 100%;
+		background-color: antiquewhite;
+	}
+	ul {
+		list-style: none;
+		display: flex;
+		gap: 5px;
+	}
+	a {
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import './my.css';
+</script>
+
+<h1>I'm a so called frontend developer</h1>
